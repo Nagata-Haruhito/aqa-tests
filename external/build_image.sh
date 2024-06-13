e545b767883c3920956c6b8e11ad6f9f8367c24a
Nagata-Haruhito <84066412+Nagata-Haruhito@users.noreply.github.com>
2024-06-13 13:52:20 +0900

    Update build_image.sh for podman debug

--- external/build_image.sh
+++ external/build_image.sh
@@ -64,7 +64,7 @@ function build_image() {
     # Used for tagging the image
     tags="adoptopenjdk-${test}-test:${version}-${package}-${os}-${vm}-${build}"
 	if [[ "$test" == *"criu"* || "$test" == *"tck"* ]]; then
-		container_build="sudo -E podman build"
+		container_build="sudo -E podman --log-level debug build"
 	fi
 
 	echo "#####################################################"
