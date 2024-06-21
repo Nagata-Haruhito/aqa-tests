3fd9eec99c4c766c5db288616b6694472e392a42
Nagata, Haruhito <nagata.haruhito@jp.fujitsu.com>
2024-06-21 16:02:03 +0900

    Revert #5356

--- external/criu-portable-checkpoint/test.sh
+++ external/criu-portable-checkpoint/test.sh
@@ -31,7 +31,6 @@ export JDK_VERSION=
 echo "JDK_VERSION has been unset, use auto-detect instead."
 export DYNAMIC_COMPILE=true
 export BUILD_LIST=functional
-echo "USE_TESTENV_PROPERTIES is $USE_TESTENV_PROPERTIES"
 
 cd /aqa-tests
 ./get.sh
--- external/dockerfile_functions.sh
+++ external/dockerfile_functions.sh
@@ -457,10 +457,10 @@ print_environment_variable() {
 
 print_home_path() {
     local file=$1
-    local aqa_tests_repo=$2
+    local github_url=$2
 
     # Get Github folder name
-    local folder="$(echo ${aqa_tests_repo} | awk -F'/' '{print $NF}' | sed 's/.git//g')"
+    local folder="$(echo ${github_url} | awk -F'/' '{print $NF}' | sed 's/.git//g')"
     echo -e "ENV TEST_HOME /${folder}\n" >> ${file}
 }
 
@@ -506,29 +506,28 @@ print_testInfo_env() {
             "\nENV JDK_VERSION=${version}" \
             "\nENV JDK_IMPL=${vm}" \
             "\n" >> ${file}
-    echo -e "\nENV USE_TESTENV_PROPERTIES ${USE_TESTENV_PROPERTIES} \n" >> ${file}
 }
 
 print_clone_project() {
     local file=$1
     local test=$2
-    local aqa_tests_repo=$3
-    local aqa_tests_branch=$4
+    local github_url=$3
 
     # Cause Test name to be capitalized
     test_tag="$(sanitize_test_names ${test} | tr a-z A-Z)_TAG"
-    # if [[ "$test_tag" != *"CRIU"* && "$test_tag" != *"TCK"* ]]; then
-    #     aqa_tests_branch=$test_tag
-    # fi
+    git_branch_tag="master"
+    if [[ "$test_tag" != *"CRIU"* && "$test_tag" != *"TCK"* ]]; then
+        git_branch_tag=$test_tag
+    fi
 
     # Get Github folder name
-    folder="$(echo ${aqa_tests_repo} | awk -F'/' '{print $NF}' | sed 's/.git//g')"
+    folder="$(echo ${github_url} | awk -F'/' '{print $NF}' | sed 's/.git//g')"
 
     echo -e "# Clone ${test} source" \
             "\nENV ${test_tag}=\$${test_tag}" \
-            "\nRUN git clone ${aqa_tests_repo}" \
+            "\nRUN git clone ${github_url}" \
             "\nWORKDIR /${folder}/" \
-            "\nRUN git checkout ${aqa_tests_branch}" \
+            "\nRUN git checkout \$${git_branch_tag}" \
             "\nWORKDIR /" \
             "\n" >> ${file}
 }
@@ -657,9 +656,9 @@ generate_dockerfile() {
         print_test_results ${file};
     fi
 
-    print_home_path ${file} ${ADOPTOPENJDK_REPO};
+    print_home_path ${file} ${github_url};
     print_testInfo_env ${test} ${tag_version} ${os} ${version} ${vm}
-    print_clone_project ${file} ${test} ${ADOPTOPENJDK_REPO} ${ADOPTOPENJDK_BRANCH} ;
+    print_clone_project ${file} ${test} ${github_url};
     print_test_files ${file} ${test} ${localPropertyFile};
 
     if [[ "$check_external_custom_test" == "1" ]]; then
